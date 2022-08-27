100c3b14e03b0bd02ff0cc276adff0ef94f07e13
anisul-islam <anisul2010s@yahoo.co.uk>
2022-08-27 23:40:04 +0300

    added react icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
-
+import { FaYoutube } from 'react-icons/fa';
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Welcome to React.</h1>
+      <p>
+        Subscribe to my <FaYoutube /> channel
+      </p>
+    </div>
+  );
 };
 
 export default App;
